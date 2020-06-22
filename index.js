73d75b090b9a44b7a4c004c6e3d4da91d8c68850
Peace <onyeepeaz@gmail.com>
2020-06-22 23:21:50 +0100

    fixed font size and added imperial temperature

--- index.js
+++ index.js
@@ -5,23 +5,29 @@ const lagoso = document.querySelector('.lagoso');
 const lekki = document.querySelector('.lekki');
 const lekki2 = document.querySelector('.lekki2');
 const header = document.querySelector('header');
+
+// weather icon
 var imgy = document.createElement('img');
 imgy.style.width = '50%';
 
+// async function
 let see = async () => {
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=metric&appid=8d56aec28d481e32e90ec65fb0a35df0`);
 
         const data = await response.json();
-    
-        console.log(data);
 
         lagoso.innerHTML = data.weather[0].main.toUpperCase();
-        lagos.innerHTML = `${Math.round(data.main.temp)}<img src="images/celsius.png" alt="celsius" style="width:3rem;">`;
-        lekki.innerHTML = `${data.name} ${data.sys.country}`;
+        let temperature = `${Math.round(data.main.temp)}`;
+        // change temperature to farenheit
+        let farenheit = Math.round((temperature * 1.8) + 32);
+        // display temperature in both celsius and farenheit
+        lagos.innerHTML = `${temperature}°C | ${farenheit}°F`;
+        lekki.innerHTML = `${data.name}, ${data.sys.country}`;
         let now  = new Date();
         lekki2.innerHTML = dateBuilder(now);
 
+        // set weather icon according to weather condition
         switch (data.weather[0].main) {
             case 'Clouds':
                 imgy.src = 'images/sky.png';
@@ -68,7 +74,7 @@ let see = async () => {
     }
     
     catch (error) {
-        alert("City not found. Try a different city.");
+        alert('City not found. Try a different city or check your internet connection.');
     }
 }
 
@@ -81,6 +87,7 @@ click.addEventListener('click', function(e) {
     lekki2.style.padding = '2rem 0';
 })
 
+// get current date
 function dateBuilder (d) {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
