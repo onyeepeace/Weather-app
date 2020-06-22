a0f56b39c4719afb36bd50915afeda5fe7722353
Peace <onyeepeaz@gmail.com>
2020-06-22 13:34:59 +0100

    better UI

--- index.js
+++ index.js
@@ -6,66 +6,68 @@ const lekki = document.querySelector('.lekki');
 const lekki2 = document.querySelector('.lekki2');
 const header = document.querySelector('header');
 var imgy = document.createElement('img');
-imgy.style.width = '30%';
+imgy.style.width = '50%';
 
 let see = async () => {
     try {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=metric&appid=8d56aec28d481e32e90ec65fb0a35df0`);
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=imperial&appid=8d56aec28d481e32e90ec65fb0a35df0`);
 
-    const data = await response.json();
-   
-    console.log(data);
-
-    lagoso.innerHTML = data.weather[0].main.toUpperCase();
-    lagos.innerHTML = `${Math.round(data.main.temp)}<img src="images/celsius.png" alt="celsius" style="width:10%;">`;
-    lekki.innerHTML = `${data.name} ${data.sys.country}`;
-    let now  = new Date();
-    lekki2.innerHTML = dateBuilder(now);
-
-    switch (data.weather[0].main) {
-        case 'Clouds':
-            imgy.src = 'images/sky.png';
-            lagoso.appendChild(imgy);
-            break;
+        const data = await response.json();
     
-        case 'Rain':
-            imgy.src = 'images/rain(1).png';
-            lagoso.appendChild(imgy);
-            break;
+        console.log(data);
 
-        case 'Thunderstorm':
-            imgy.src = 'images/storm.png';
-            lagoso.appendChild(imgy);
-            break;
+        lagoso.innerHTML = data.weather[0].main.toUpperCase();
+        lagos.innerHTML = `${Math.round(data.main.temp)}<img src="images/celsius.png" alt="celsius" style="width:10%;">`;
+        lekki.innerHTML = `${data.name} ${data.sys.country}`;
+        let now  = new Date();
+        lekki2.innerHTML = dateBuilder(now);
 
-        case 'Clear':
-            imgy.src = 'images/sun.png';
-            lagoso.appendChild(imgy);
-            break;
+        switch (data.weather[0].main) {
+            case 'Clouds':
+                imgy.src = 'images/sky.png';
+                lagoso.appendChild(imgy);
+                break;
+        
+            case 'Rain':
+                imgy.src = 'images/rain(1).png';
+                lagoso.appendChild(imgy);
+                break;
 
-        case 'Smoke':
-            imgy.src = 'images/wind.png';
-            lagoso.appendChild(imgy);
-            break;
+            case 'Thunderstorm':
+                imgy.src = 'images/storm.png';
+                lagoso.appendChild(imgy);
+                break;
 
-        case 'Drizzle':
-            imgy.src = 'images/rain(4).png';
-            lagoso.appendChild(imgy);
-            break;
+            case 'Clear':
+                imgy.src = 'images/sun.png';
+                lagoso.appendChild(imgy);
+                break;
 
-        case 'Snow':
-            imgy.src = 'images/snow.png';
-            lagoso.appendChild(imgy);
-            break;
+            case 'Smoke':
+                imgy.src = 'images/wind.png';
+                lagoso.appendChild(imgy);
+                break;
 
-        default:
-            imgy.src = 'images/sun(1).png';
-            lagoso.appendChild(imgy);
-            break;
-            //mist haze
+            case 'Drizzle':
+                imgy.src = 'images/rain(4).png';
+                lagoso.appendChild(imgy);
+                break;
+
+            case 'Snow':
+                imgy.src = 'images/snow.png';
+                lagoso.appendChild(imgy);
+                break;
+
+            default:
+                imgy.src = 'images/sun(1).png';
+                lagoso.appendChild(imgy);
+                break;
+        }
+
+        return data;
     }
-    return data;
-    } catch (error) {
+    
+    catch (error) {
         alert("City not found. Try a different city.");
     }
 }
@@ -75,8 +77,8 @@ click.addEventListener('click', function(e) {
     see();
     city.value = '';
     header.style.display = 'none';
-    // document.body.style.display = 'block';
-    // document.body.style.margin = '1px 2rem';
+    lagoso.style.paddingTop = '2rem';
+    lekki2.style.padding = '2rem 0';
 })
 
 function dateBuilder (d) {
